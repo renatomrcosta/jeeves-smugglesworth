3e701e2ea9b7239bf6c42b88c87778a766c673cf
Renato Costa <renato.rezende.costa@gmail.com>
2018-10-24 20:36:11 +0200

    I001 - Setup of a simple firebase store

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var RTMClient = require('@slack/client').RTMClient;
-var rtm = new RTMClient('');
+var serviceAccountKey = require('./serviceAccountKey.json');
+var firebase = require('firebase');
+var slackConf = require(';/slack.json');
+var rtm = new RTMClient(slackConf.api_token);
+var firebaseApp = firebase.initializeApp(serviceAccountKey);
+var db = firebaseApp.firestore();
 rtm.start();
 rtm.on("message", function (message) {
     if (message.text.toLowerCase().includes("merge")) {
+        db.collection("merge").add({
+            text: message.text,
+            msg: message
+        });
         rtm.sendMessage("Olha o merge aí negada, <@" + message.user + "> has something important to say!", message.channel);
     }
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 const { RTMClient } = require('@slack/client');
+const serviceAccountKey = require('./serviceAccountKey.json');
+const firebase = require('firebase');
+const slackConf = require(';/slack.json')
 
-const rtm = new RTMClient('');
+
+const rtm = new RTMClient(slackConf.api_token);
+const firebaseApp = firebase.initializeApp(serviceAccountKey);
+const db = firebaseApp.firestore();
 
 rtm.start();
 
 rtm.on("message", function(message) {
     if(message.text.toLowerCase().includes("merge")){
+        db.collection(`merge`).add({
+            text: message.text,
+            msg: message
+        });
         rtm.sendMessage("Olha o merge aí negada, <@" + message.user + "> has something important to say!", message.channel);
     }
 });
