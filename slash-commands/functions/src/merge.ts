724ae175483f10d9de05b0d4a85298f9e8ff55c5
Ronaldo Correa <ronaldo.correa@fornax.com.br>
2018-10-27 09:39:23 +0200

    add response obj

--- slash-commands/functions/src/merge.ts
+++ slash-commands/functions/src/merge.ts
@@ -1,14 +1,14 @@
 import * as functions from 'firebase-functions';
 import * as bodyparser from 'body-parser'
-import {ResponseSlashCommand} from "./response.slash.command";
+import {InChannelResponse} from "./response.slash.command";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 
 export const merge = functions.https.onRequest((request, response) => {
-    const responseSlashCommand = new ResponseSlashCommand("in_channel");
-    responseSlashCommand.text = "It's too early <@" + request.body.user_id + "> ... come back here later";
-    response.send(responseSlashCommand)
+    const inChannelResponse = new InChannelResponse();
+    inChannelResponse.text = "It's too early <@" + request.body.user_id + "> ... come back here later";
+    response.send(inChannelResponse)
 
 });
--- slash-commands/functions/src/response.slash.command.ts
+++ slash-commands/functions/src/response.slash.command.ts
@@ -1,9 +1,17 @@
-export class ResponseSlashCommand {
+class ResponseSlashCommand {
     response_type: string;
     text: string;
 
     constructor(response_type:string){
         this.response_type = response_type;
     }
-
 }
+class InChannelResponse extends ResponseSlashCommand {
+    constructor(){
+        super("in_channel");
+    }
+}
+export {ResponseSlashCommand, InChannelResponse}
+
+
+
