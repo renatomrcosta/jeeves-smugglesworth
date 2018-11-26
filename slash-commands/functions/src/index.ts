f342620d02d06de8f178295fd1878d2463f81813
Renato Costa <renato.costa@cipalschaubroeck.be>
2018-11-26 12:39:44 +0100

    Added messages. Adjusted build scripts

--- slash-commands/functions/src/index.ts
+++ slash-commands/functions/src/index.ts
@@ -1,8 +1,6 @@
 import { done } from './done';
 import { merge } from './merge';
 import { status } from './status';
-import * as firebase from "firebase-admin";
-
 
 
 export {done, merge, status}
--- slash-commands/functions/src/merge.ts
+++ slash-commands/functions/src/merge.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as messages from '../resources/messages'
 
 import {parseSlashCommandPostContent, PostContent, InChannelResponse} from "./slash-command.common";
 
@@ -21,18 +22,17 @@ export const merge = functions.https.onRequest((request, response) => {
             }
             return channels.docs[0].id;
         }).then((channelId)=>{
-
-            return firestore.collection(`channels`).doc(channelId).collection("queue").where("user_id","==",postContent.user_id).get().then((userInQueue)=>{
+            return firestore.collection(`channels`).doc(channelId).collection("queue").get().then((snapshot) => {
+            // return firestore.collection(`channels`).doc(channelId).collection("queue").where("user_id","==",postContent.user_id).get().then((userInQueue)=>{
                 const inChannelResponse = new InChannelResponse();
-                if (userInQueue.empty) {
+                //counting size for small collections
+                if (snapshot.size == 0) {
                     return firestore.collection(`channels`).doc(channelId).collection("queue").add({user_id:postContent.user_id}).then((data)=>{
-
-                        inChannelResponse.text = `Ok <@${postContent.user_id}> you can start your merge!! BANANA!`;
+                        inChannelResponse.text = messages.firstInLine.replace('%s', postContent.user_id);
                         return inChannelResponse
                     });
                 }
-
-                inChannelResponse.text = `Hold on a sec <@${postContent.user_id}>  ... It seems asdasd you are already in the line for merge`;
+                inChannelResponse.text = messages.waitForABit.replace('%s', postContent.user_id);
                 return inChannelResponse;
             });
         }).then((userInQueue)=>{
