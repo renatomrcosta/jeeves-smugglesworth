ef96de1c4cbfadc7b6e551ccf7f4e076e0170288
Renato Costa <renato.rezende.costa@gmail.com>
2018-12-23 13:00:12 +0100

    Cleared up the commented code

--- src/jeeves.ts
+++ src/jeeves.ts
@@ -54,26 +54,3 @@ module.exports = {
   app: app,
   jeeves: api
 };
-
-// const { RTMClient } = require('@slack/client');
-// const {chat} = require('@slack/client');
-// const serviceAccountKey = require('./serviceAccountKey.json');
-// const firebase = require('firebase');
-// const slackConf = require(';/slack.json');
-//
-//
-// const rtm = new RTMClient(slackConf.api_token);
-// const firebaseApp = firebase.initializeApp(serviceAccountKey);
-// const db = firebaseApp.firestore();
-//
-// rtm.start();
-//
-// rtm.on("message", function(message) {
-//     if(message.text.toLowerCase().includes("merge")){
-//         db.collection(`merge`).add({
-//             text: message.text,
-//             msg: message
-//         });
-//         rtm.sendMessage("Olha o merge aí negada, <@" + message.user + "> has something important to say!", message.channel);
-//     }
-// });
