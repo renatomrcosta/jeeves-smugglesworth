00b5d904299403061c0907ad0b6720f24af121eb
Renato Costa <renato.rezende.costa@gmail.com>
2018-12-24 14:00:11 +0100

    provisioning the API Tokens via process rather than json files

--- src/firebase.init.ts
+++ src/firebase.init.ts
@@ -1,5 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+};
+
+console.log(serviceAccount);
 
 //On startup, initializes the firebase config for the app
 admin.initializeApp({
