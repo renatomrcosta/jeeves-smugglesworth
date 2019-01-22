0d47ebec322f1aaa6c263c6b624d03f4f0dd8371
Renato Costa <renato.costa@cipalschaubroeck.be>
2019-01-22 09:29:33 +0100

    Updated typescript, imported json

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,5 @@
 import messageService from '../services/messages.service';
-const messageList = require('../messages.json');
+import messageList from '../messages.json';
 
 const helpCommand = (payload: Payload) => {
     messageService.sendSlackMessageByObject(payload.event.channel, messageList.help);
