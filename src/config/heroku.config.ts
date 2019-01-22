0ee635dfadfd99d7f95114973d712891c6cd1374
Renato Costa <renato.costa@cipalschaubroeck.be>
2019-01-22 09:04:32 +0100

    Slight adjustment of the configuration files

--- src/config/heroku.config.ts
+++ src/config/heroku.config.ts
@@ -1,19 +1,18 @@
-// const https = require('https');
-//
-// const url = process.env.HEROKU_KEEP_ALIVE_URL + 'keep-alive';
-//
-// const KEEP_ALIVE_TIMER = 600000;
-//
-// const keepHerokuAlive = () => {
-//     if(url){
-//         setInterval(() => {
-//             https.get(url,(response) => {
-//                 console.log(`Keep Alive on ${url} triggered at`, new Date(), "Status code: " ,response.statusCode);
-//             });
-//         }, KEEP_ALIVE_TIMER);
-//     }
-// };
-//
-// module.exports = {
-//     config: keepHerokuAlive
-// };
+import https from 'https';
+
+const URL = process.env.HEROKU_KEEP_ALIVE_URL + 'keep-alive';
+const KEEP_ALIVE_TIMER = 600000;
+
+const keepHerokuAlive = () => {
+    if(URL){
+        setInterval(() => {
+            https.get(URL,(response) => {
+                console.log(`Keep Alive on ${URL} triggered at`, new Date(), "Status code: " ,response.statusCode);
+            });
+        }, KEEP_ALIVE_TIMER);
+    }
+};
+
+module.exports = {
+    config: keepHerokuAlive
+};
