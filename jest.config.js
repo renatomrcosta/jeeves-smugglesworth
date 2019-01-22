db33f037dbba8b9226576cb4250a627cea84cbb6
Renato Costa <renato.costa@cipalschaubroeck.be>
2019-01-22 12:16:14 +0100

    Updated Jest config to suppress warnings

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,7 @@
 module.exports = {
-    globals: {
-        "ts-jest": {
-            tsConfigFile: "tsconfig.json",
-        },
-    },
-    moduleFileExtensions: [
-        "ts",
-        "json",
-        "js",
-    ],
-    testEnvironment: "node",
-    testMatch: [
-        "**/test/**/*.(test|spec).(ts|js)",
-    ],
-    transform: {
-        "^.+\\.(ts|tsx)$": "./node_modules/ts-jest/preprocessor.js",
-    },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    "**/test/**/*.(test|spec).(ts|js)",
+  ],
 };
