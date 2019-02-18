3ad51925bb8e381ade39a87125dde510fa4c079c
Renato Costa <renato.costa@cipalschaubroeck.be>
2019-02-18 16:14:34 +0100

    Rewrote the threshold for hours

--- src/handlers/time_warning.ts
+++ src/handlers/time_warning.ts
@@ -5,28 +5,33 @@ import queueService from "../services/queue.service";
 import {interval} from "rxjs";
 
 // Three hour warning threshold, initially.
-const WARNING_THRESHOLD = 60 * 1000;
-
-
+const WARNING_HOUR_THRESHOLD = 3;
 /***
  * The time warning event, requested in issue #30; Once every hour, it will query the top of the queues and send a
  * warning to whoever is in the queue more than 3h.
  * */
 const handleEvent = () => {
-    console.log("Configured the time warning with the following Threshold", WARNING_THRESHOLD);
+    console.log("Configured the time warning with the following Hour Threshold", WARNING_HOUR_THRESHOLD);
 
-    const warningInterval = interval(60000);
+    const warningInterval = interval(15 * 1000);
     warningInterval.subscribe(() => {
-        // Query the open queues
-
-        const dateThreshold = Date.now() - WARNING_THRESHOLD;
-        // Send a warning per top queue that is more than 3h old.
         queueService
-            .getQueues()
-            .filter( (queue: IQueue)  => +queue.queue_timestamp < dateThreshold)
-            .forEach( (queue: IQueue) => {
-                messageService.sendMessage(queue.channel_id,
-                    messageList.timeoutWarning.replace("%s", messageService.mentionSlackUser(queue.user_id)));
+            .getQueuesByHour(WARNING_HOUR_THRESHOLD)
+            .then( (channelSnapshot: any) => {
+                if(!channelSnapshot.empty) {
+                    channelSnapshot.docs
+                        .map( (documentSnapshot: any) => {
+                            return {
+                                channel_id: documentSnapshot.get("channel_id"),
+                                user_id: documentSnapshot.get("user_id"),
+                            };
+                        })
+                        .forEach( (queue: IQueue) => {
+                            messageService.sendMessage(queue.channel_id,
+                                messageList.timeoutWarning
+                                    .replace("%s", messageService.mentionSlackUser(queue.user_id)));
+                    });
+                }
             });
     });
 };
